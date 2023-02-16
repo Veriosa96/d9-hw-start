99a0f6f2ffb05fee3afdce2420001c13fcff6572
Saverio Caracappa <saveriocaracappa@gmail.com>
2023-02-16 14:49:26 +0100

    start project

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,11 @@
+const inizialState = {
+    job:{
+        favorite: []
+    }
+}
+// stato iniziale che troviamo ogni volta all'apertura della pagina prima di essere sostituito da un altro stato compilato
+
+const firstReducer = (state = inizialState, action) => {
+
+}
+
